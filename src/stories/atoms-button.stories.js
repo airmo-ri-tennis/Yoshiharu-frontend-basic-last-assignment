53bd14fb921b4ef26da2bd62f643976a39b79e2c
Yoshiharu Moriya <y.moriya@4nonome.com>
2021-09-10 16:52:18 +0900

    initial commit. Atoms-Button

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,8 @@
+/* src/stories/atoms-example.stories.js */
+
+import Atoms_Button from "../components/atoms/button/button.html";
+import "../components/atoms/button/button.scss";
+
+export default { title: "Atoms/Button" };
+
+export const Button = () => Atoms_Button;
